265e79b92b3a17f56fdca048d031e848f9778c92
Marcel Cruz <marcelcruz@outlook.com>
2021-11-22 19:40:14 -0500

    Solucao Digo
    Co-authored-by: RodrigoTardeo <RodrigoTardeo@users.noreply.github.com>

--- src/fooBarQix.test.ts
+++ src/fooBarQix.test.ts
@@ -7,7 +7,65 @@ describe("", () => {
     _sut = new FooBarQix();
   });
 
-  it("Primeiro test", () => {
-    expect(_sut.FooMyNumber(6)).toBe("");
+
+  it("Should return 1 when called with 1", () => {
+    expect(_sut.FooMyNumber(1)).toBe("1");
   });
+
+  it("Should return 2 when called with 2", () => {
+    expect(_sut.FooMyNumber(2)).toBe("2");
+  });
+
+  it("Should return 4 when called with 4", () => {
+    expect(_sut.FooMyNumber(4)).toBe("4");
+  });
+
+
+  it("Should return Foo when called with 6", () => {
+    expect(_sut.FooMyNumber(6)).toBe("Foo");
+  });
+
+  it("Should return FooFoo when called with 3", () => {
+    expect(_sut.FooMyNumber(3)).toBe("FooFoo");
+  });
+
+  it("Should return BarBar when called with 5", () => {
+    expect(_sut.FooMyNumber(5)).toBe("BarBar");
+  });
+
+  it("Should return QixQix when called with 7", () => {
+    expect(_sut.FooMyNumber(7)).toBe("QixQix");
+  });
+
+  it("Should return Bar when called with 10", () => {
+    expect(_sut.FooMyNumber(10)).toBe("Bar");
+  });
+
+  it("Should return FooBarBar when called with 15", () => {
+    expect(_sut.FooMyNumber(15)).toBe("FooBarBar");
+  });
+
+  it("Should return Bar when called with 20", () => {
+    expect(_sut.FooMyNumber(20)).toBe("Bar");
+  });
+
+  it("Should return FooQix when called with 21", () => {
+    expect(_sut.FooMyNumber(21)).toBe("FooQix");
+  });
+
+  it("Should return Qix when called with 28", () => {
+    expect(_sut.FooMyNumber(28)).toBe("Qix");
+  });
+  it("Should return Qix when called with 49", () => {
+    expect(_sut.FooMyNumber(49)).toBe("Qix");
+  });
+
+  it("Should return FooFooFoo when called with 33", () => {
+    expect(_sut.FooMyNumber(33)).toBe("FooFooFoo");
+  });
+
+  it("Should return FooQix when called with 27", () => {
+    expect(_sut.FooMyNumber(27)).toBe("FooQix");
+  });
+ 
 });
--- src/fooBarQix.ts
+++ src/fooBarQix.ts
@@ -1,5 +1,40 @@
 export class FooBarQix {
   FooMyNumber(numero: number): string {
-    return "";
+    // let Div3: number
+    let char: string = "";
+    
+  
+    if (numero % 3 === 0) {
+      char = "Foo"
+    }
+    if (numero % 5 === 0) {
+      char = char + "Bar";
+    } 
+    if (numero % 7 === 0) {
+      char = char + "Qix";
+    }
+  if (numero === 3) {
+      return char + "Foo";
+    }
+    if (numero === 5) {
+      return char + "Bar";
+    }
+    if (numero === 7) {
+      return char + "Qix";
+    }
+    if (numero === 15) {
+      return char + "Bar";
+    }
+    if (numero === 27) {
+      return char + "Qix";
+    }
+    if (numero === 33) {
+      return char + "FooFoo";
+    }
+    if (char === "") {
+      return numero.toString();
+    }
+
+    return char;
   }
 }
